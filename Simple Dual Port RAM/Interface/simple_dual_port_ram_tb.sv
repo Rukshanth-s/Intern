07c81be403fd6ebd721a3a92be8dffcfb4134b51
Rukshanth-s <83012559+Rukshanth-s@users.noreply.github.com>
2023-01-27 18:59:20 +0530

    Randomized testing and checking

--- Simple Dual Port RAM/Interface/simple_dual_port_ram_tb.sv
+++ Simple Dual Port RAM/Interface/simple_dual_port_ram_tb.sv
@@ -1,37 +1,54 @@
 `include "interface.sv";
-
+`include "generator.sv";
 module simple_dual_port_ram_tb();
    logic clk;
    logic rst;
-  localparam clk_period =1000;
-   //sdpram_if #(32, 1024, 0) sdpram_inst(.clk(clk),.rst(rst),.sdp_s(ifp));
-  sdpram_if #(32,1024,0) sdpram_if_inst();
-
- simple_dual_port_ram  dut(
-    .clk(clk),
-    .rst(rst),
-         .ifp(sdpram_if_inst)
-);
-/* initial begin
-       clk=1'b1;
-       forever #5 clk=~clk;
-     end*/
+   localparam CLK_PERIOD = 10;
+  	localparam repititions =100;
+  generator g;
+   sdpram_if #(32,1024,0) sdpram_if_inst();
+  simple_dual_port_ram  simple_dual_port_ram_inst(.clk(clk),.rst(rst),.ifp(sdpram_if_inst));
+  
+  always #(CLK_PERIOD/2) clk=~clk;
   initial begin
-    clk = 1'b1;
-end
+    clk=1'b1;
+  end
+    
+  logic [sdpram_if_inst.DATA_WIDTH-1:0] mem_chk [sdpram_if_inst.MEM_DEPTH];
+  initial begin
+    g=new();
+     for(int i=0;i<repititions;i++) begin
+       randomize(g.addra) with {g.addra inside{ [0:2**sdpram_if_inst.ADDR_WIDTH]};};
+       //randomize(g.dina) with {g.dina inside{ [0:2**10]};};
+       randomize(g.wena) with {g.wena inside{ [0:2**sdpram_if_inst.STRB_WIDTH]};};
+      
+   
+      g.dina = $random;
 
-always begin
-    #(clk_period/2) clk = ~clk;
-end
-
-            
-     initial begin
-       sdpram_if_inst.dina= 32'h 55;
-       sdpram_if_inst.addra= 10'h 01;
-       sdpram_if_inst.wena =1;
-       sdpram_if_inst.renb =1;
-       sdpram_if_inst.addrb =10'h 01;
-     end
+      #(CLK_PERIOD *2);
+      sdpram_if_inst.wena = g.wena;
+sdpram_if_inst.addra = g.addra;
+sdpram_if_inst.dina= g.dina;
+      if (g.wena)
+        mem_chk[g.addra] = g.dina;  
+    end
+                                     end
+  
+  initial begin 
+    g=new();
+    for(int i=0; i<repititions;i++) begin
+      
+      randomize(g.addrb) with {g.addrb inside{ [0:2**sdpram_if_inst.ADDR_WIDTH]};};
+      #(CLK_PERIOD *2);
+      sdpram_if_inst.renb=1;
+      sdpram_if_inst.addrb = g.addrb;
+      @(posedge clk);
+	  # (CLK_PERIOD * 2.5);
+      a1 :assert (mem_chk[g.addrb]===sdpram_if_inst.doutb ) $display("[PASS] Addr = %0h,\n \t   Data :: Expected = %0h Actual = %0h",g.addrb,mem_chk[g.addrb],sdpram_if_inst.doutb);  //assertion to check whether read data is correct
+      else $error("[FAIL] Addr = %0h,\n \t   Data :: Expected = %0h Actual = %0h",g.addrb,mem_chk[g.addrb],sdpram_if_inst.doutb);
+    end
+  end
+  
   
   initial begin
     $dumpvars;
@@ -39,9 +56,8 @@ end
     
   end
     initial begin
-    #6000
-    $finish; 
-    end
+    #60000
+      $finish; 
     
-  
-endmodule
+ end
+ endmodule
