92a03264afd5b0294a46a738be86da70a8170cce
Rukshanth-s <83012559+Rukshanth-s@users.noreply.github.com>
2023-02-06 16:28:32 +0530

    Code style alignment and logic data type.

--- Simple Dual Port RAM/Interface/generator.sv
+++ Simple Dual Port RAM/Interface/generator.sv
@@ -1,19 +1,14 @@
 class generator;
-   parameter DATA_WIDTH = 32;
-
-        parameter MEM_DEPTH  = 1024;      
-
-       parameter BYTE_WRITE = 0;
-   localparam ADDR_WIDTH = $clog2(MEM_DEPTH);
-
-  localparam STRB_WIDTH = BYTE_WRITE ? (DATA_WIDTH/8) : 1;
-
-    bit [ADDR_WIDTH-1:0]  addra;   
- bit    wena;
- bit [DATA_WIDTH-1:0]  dina;  
- bit [ADDR_WIDTH-1:0]  addrb;   
-  bit   renb;
-  bit [DATA_WIDTH-1:0]  doutb;
- bit    dvalb;
- 
-  endclass
+	parameter DATA_WIDTH = 32;
+	parameter MEM_DEPTH  = 1024;      
+	parameter BYTE_WRITE = 0;
+	localparam ADDR_WIDTH = $clog2(MEM_DEPTH);
+	localparam STRB_WIDTH = BYTE_WRITE ? (DATA_WIDTH/8) : 1;
+	logic [ADDR_WIDTH-1:0]  addra;   
+	logic    wena;
+	logic [DATA_WIDTH-1:0]  dina;  
+	logic [ADDR_WIDTH-1:0]  addrb;   
+	logic   renb;
+	logic [DATA_WIDTH-1:0]  doutb;
+	logic    dvalb;
+endclass
